7da9070c0bd7ef3328b486ea5a785f85f401dc65
Kevin Marshall <info@falicon.com>
2018-10-04 14:47:01 -0400

    platform stuff

--- app/app.js
+++ app/app.js
@@ -1,34 +1,27 @@
 'use strict';
 
-// =================================================================================
-// App Configuration
-// =================================================================================
-
 const {App} = require('jovo-framework');
 
 const config = {
-    logging: true,
+  logging: true,
 };
 
 const app = new App(config);
 
-
-// =================================================================================
-// App Logic
-// =================================================================================
-
 app.setHandler({
-    'LAUNCH': function() {
-        this.toIntent('HelloWorldIntent');
-    },
 
-    'HelloWorldIntent': function() {
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
+  'LAUNCH': function() {
+    this.toIntent('HelloWorldIntent');
+  },
 
-    'MyNameIsIntent': function(name) {
-        this.tell('Hey ' + name.value + ', nice to meet you!');
-    },
+  'HelloWorldIntent': function() {
+    this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+  },
+
+  'MyNameIsIntent': function(name) {
+    this.tell('Hey ' + name.value + ', nice to meet you!');
+  },
+
 });
 
 module.exports.app = app;
